e111c2ca7da4499c6fd85736c3f5aef7a794abe3
Evgeni Ruban <ru8.evgeny@yandex.ru>
2025-09-09 23:31:16 +0300

    my commit - edit val name

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,7 @@ package ru.stimax
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
+    val name = "Evgeni"
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     println("Hello, " + name + "!")
