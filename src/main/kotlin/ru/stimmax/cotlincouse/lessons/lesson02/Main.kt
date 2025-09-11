2d277a55e84e1716fdbfd5da416834dc32129e16
Evgeni Ruban <ru8.evgeny@yandex.ru>
2025-09-11 23:33:53 +0300

    my commit -  add homework03

--- src/main/kotlin/ru/stimmax/cotlincouse/lessons/lesson02/Main.kt
+++ src/main/kotlin/ru/stimmax/cotlincouse/lessons/lesson02/Main.kt
@@ -1,10 +1,10 @@
 package ru.stimmax.cotlincouse.lessons.lesson02
 
 fun main() {
-    val name = "Evgeni"
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        println("i = $i")
-    }
+}
+
+fun test() {
+    var test: String = ""
+
 }
--- src/main/kotlin/ru/stimmax/cotlincouse/lessons/lesson03/Lesson03.kt
+++ src/main/kotlin/ru/stimmax/cotlincouse/lessons/lesson03/Lesson03.kt
@@ -1,5 +1,7 @@
 package ru.stimmax.cotlincouse.lessons.lesson03
 
+const val wheels: Int = 4
+
 class Lesson03 {
 
     val name: String = "Evgeni"// инициализировали переменную
@@ -16,8 +18,55 @@ class Lesson03 {
         "Hello, this is a lazy string!"
     }
 
-    var score: Int = 10
+    var score: Int? = 10
     var speed: Double? = null // null отсутствие значения. Нужно обозначить тип как  ?
+
+    fun example1() {
+        speed?.dec()// проверили с помощью ?, что тип может быть null
+        score?.dec()
+        count = 10
+    }
+
+
+    var count: Int = 0 // ограничивают работу с переменной
+        get() {
+            return if (field > 100) field else 0
+        }
+        set(value) {
+            if (value >= 0) field = value
+        }
+    // get - регулирует то как будем получать значения из переменной
+    // set - в него передается какое-то значение и он будет определять каким образом записать значение в переменную
+
+
+
+    val vinCode: String = "ERTYUIOP%^&*"
+
+    var colorCar: String = "RED"
+
+    var mileAge: Int = 0
+
+    lateinit var ownerName : String
+
+    val utilReport: String by lazy {
+        ""
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
