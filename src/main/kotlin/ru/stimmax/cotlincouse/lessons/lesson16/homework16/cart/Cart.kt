828a7bdd3d86457ac778fa3f83afb97894ffc577
Evgeni Ruban <ru8.evgeny@yandex.ru>
2025-10-30 19:54:35 +0300

    my commit -  add homework16

---
+++ src/main/kotlin/ru/stimmax/cotlincouse/lessons/lesson16/homework16/cart/Cart.kt
@@ -0,0 +1,59 @@
+package ru.stimmax.cotlincouse.lessons.lesson16.homework16.cart
+
+class Cart {
+    val item = mutableMapOf<String, Int>()
+
+    fun addToCart(itemId: String) {
+        item[itemId] = 1
+    }
+
+    //Выполнить перегрузку addToCart который:
+    //Принимает два аргумента (itemId и количество amount)
+    //Учесть, что если товар уже есть в корзине, нужно увеличить его количество, если нет - добавить.
+    fun addToCart(itemId: String, amount: Int) {
+        if (item.containsKey(itemId)) {
+            item[itemId] = (item[itemId] ?: 0) + amount
+        } else {
+            item[itemId] = amount
+        }
+    }
+
+
+    //Принимает словарь из id и количества и добавляет всё в корзину
+    //Учесть, что если товар уже есть в корзине, нужно увеличить его количество, если нет - добавить.
+    fun addToCart(itemMap: Map<String, Int>) {
+        for ((key, values) in itemMap) {
+            if (item.containsKey(key)) {
+                item[key] = (item[key] ?: 0) + values
+            } else {
+                item[key] = values
+            }
+
+        }
+
+    }
+
+    // Принимает список из id (добавляет по одной единице).
+    //Учесть, что если товар уже есть в корзине, нужно увеличить его количество, если нет - добавить.
+    fun addToCart(itemId: List<String>) {
+        for (i in itemId) {
+            if (item.containsKey(i)) {
+                item[i] = (item[i] ?: 0) + 1
+            } else {
+                item[i] = 1
+            }
+
+        }
+    }
+
+    fun toStrings(color: String, items: Map<String, Int>) {
+        val result = """
+            $color
+            Количество товара: ${item.keys.count()}
+            Количество единиц товара: ${item.values.sum()}
+        """.trimIndent()
+        println(result)
+    }
+
+
+}
---
+++ src/main/kotlin/ru/stimmax/cotlincouse/lessons/lesson16/homework16/cart/Main.kt
@@ -0,0 +1,18 @@
+package ru.stimmax.cotlincouse.lessons.lesson16.homework16.cart
+
+import ru.stimmax.cotlincouse.lessons.lesson16.Colors
+import ru.stimmax.cotlincouse.lessons.lesson16.homework16.printer.Background
+
+fun main() {
+    val cart = Cart()
+    cart.addToCart("Товар1")
+    cart.addToCart("Товар1", 3)
+    cart.addToCart(listOf("Товар2", "Товар3", "Товар4"))
+    cart.addToCart(mapOf("Товар" to 1, "Товар6" to 3))
+    println(cart.item)
+
+    cart.toStrings(Colors.RED, cart.item)
+
+
+
+}
